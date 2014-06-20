5a5c16b01118b9cf127e67bd977b31ee48e6460d
Noam Gonen <me@noamgonen.com>
2014-06-20 04:44:33 -0700

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -32,6 +32,7 @@
 
 	    <form action="/sign" method="post">
 	      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+	      <div><h1>WHAT TITLE !!</h1></div>
 	      <div><input type="submit" value="Sign Guestbook"></div>
 	    </form>
 
