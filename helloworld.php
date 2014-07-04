20cd5f679ddbfab327ef28e41dbb570573bb707e
Noam Gonen <me@noamgonen.com>
2014-07-04 03:04:19 -0700

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -20,7 +20,7 @@
   	<body>
 	    <?php
 		    if ($user) {
-				echo 'GOODBYE, ' . htmlspecialchars($user->getNickname());
+				echo 'GOODBYE, WORLD' . htmlspecialchars($user->getNickname());
 
 				if (array_key_exists('content', $_POST)) {
 					echo "<br/><br/>";
